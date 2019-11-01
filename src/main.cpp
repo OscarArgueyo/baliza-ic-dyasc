7edc405997462cb112953273376487f2b6994668
Oscar Argueyo <oscar.argueyo@telefonica.com>
2019-10-31 21:16:00 -0300

    ultimos cambios, intento de pegarle a travis

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-
+#include <ArduinoJson.h>
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <WebServer.h>
@@ -11,6 +11,18 @@ WebServer server(80);
 //Enter your WiFi SSID and PASSWORD
 const char* ssid = "TeleCentro-81e1";
 const char* password = "RDMWEJNXMWY2";
+
+int status = WL_IDLE_STATUS;
+// if you don't want to use DNS (and reduce your sketch size)
+// use the numeric IP instead of the name for the server:
+//IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
+char web[] = "www.google.com";    // name address for Google (using DNS)
+
+// Initialize the Ethernet client library
+// with the IP address and port of the server
+// that you want to connect to (port 80 is default for HTTP):
+WiFiClient client;
+
 
 //===============================================================
 // This routine is executed when you open its IP in browser
@@ -61,10 +73,72 @@ void setup(void){
   Serial.println("HTTP server started");
 }
 
+void getTravisInfo(){
+  client.setTimeout(10000);
+  if (!client.connect("api.travis-ci.org", 80)) {
+    Serial.println(F("Connection failed"));
+    return;
+  }
+
+  Serial.println(F("Connected!"));
+
+  // Send HTTP request
+  client.println(F("GET /repos HTTP/1.0"));
+  client.println(F("Host: api.travis-ci.org"));
+  client.println(F("User-Agent: API Explorer"));
+  client.println(F("Authorization: token pQ4GsR2ok8LmbhzmC7a2cA"));
+  client.println(F("Travis-API-Version: 3"));
+  if (client.println() == 0) {
+    Serial.println(F("Failed to send request"));
+    return;
+  }
+
+  // Check HTTP status
+  char status[32] = {0};
+  client.readBytesUntil('\r', status, sizeof(status));
+  if (strcmp(status, "HTTP/1.1 200 OK") != 0) {
+    Serial.print(F("Unexpected response: "));
+    Serial.println(status);
+    return;
+  }
+
+  // Skip HTTP headers
+  char endOfHeaders[] = "\r\n\r\n";
+  if (!client.find(endOfHeaders)) {
+    Serial.println(F("Invalid response"));
+    return;
+  }
+
+  // Allocate the JSON document
+  // Use arduinojson.org/v6/assistant to compute the capacity.
+  const size_t capacity = JSON_OBJECT_SIZE(3) + JSON_ARRAY_SIZE(2) + 60;
+  DynamicJsonDocument doc(capacity);
+
+  // Parse JSON object
+  DeserializationError error = deserializeJson(doc, client);
+  if (error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.c_str());
+    return;
+  }
+
+  // Extract values
+  Serial.println(F("Response:"));
+  Serial.println(doc["sensor"].as<char*>());
+  Serial.println(doc["time"].as<long>());
+  Serial.println(doc["data"][0].as<float>(), 6);
+  Serial.println(doc["data"][1].as<float>(), 6);
+
+  // Disconnect
+  client.stop();
+}
+
+
 //===============================================================
 // This routine is executed when you open its IP in browser
 //===============================================================
 void loop(void){
   server.handleClient();
-  delay(1);
+  getTravisInfo();
+  delay(1000);
 }
