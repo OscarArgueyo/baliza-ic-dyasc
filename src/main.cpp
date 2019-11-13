cbc82b0b42a19cf3e4734cf9695d67ba361c4b14
Oscar Argueyo <oscar.argueyo@telefonica.com>
2019-11-12 22:13:15 -0300

    van los cambios

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,12 @@
 #include <Arduino.h>
 #include <ArduinoJson.h>
-#include <WiFi.h>
-#include <WiFiClient.h>
+#include <IotWebConf.h>
 #include <HTTPClient.h>
-#include <WebServer.h>
-
 #include <WifiManager.hpp>
 
 #include <Adafruit_NeoPixel.h>
-  #include "index.h"  //Web page header file
 
-WebServer server(80);
+
 // Which pin on the Arduino is connected to the NeoPixels?
 #define PIN 14 // On Trinket or Gemma, suggest changing this to 1
 
@@ -29,77 +25,119 @@ Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 //Enter your WiFi SSID and PASSWORD
 //const char* ssid = "IplanLiv-181329-2.4GHz";
-const char* ssid = "TeleCentro-81e1";
+//const char* ssid = "TeleCentro-81e1";
 //const char* password = "37226269";
-const char* password = "RDMWEJNXMWY2";
+//const char* password = "RDMWEJNXMWY2";
 
-int status = WL_IDLE_STATUS;
-// if you don't want to use DNS (and reduce your sketch size)
-// use the numeric IP instead of the name for the server:
-//IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
-char web[] = "www.google.com";    // name address for Google (using DNS)
+// -- Initial name of the Thing. Used e.g. as SSID of the own Access Point.
+const char thingName[] = "BALIZA";
 
-// Initialize the Ethernet client library
-// with the IP address and port of the server
-// that you want to connect to (port 80 is default for HTTP):
-WiFiClient client;
-HTTPClient httpClient;
+// -- Initial password to connect to the Thing, when it creates an own Access Point.
+const char wifiInitialApPassword[] = "dyasc2019";
+
+boolean WIFI_SUCCESSFUL_CONNECTION = false;
+
+#define STRING_LEN 255
+#define NUMBER_LEN 32
+
+// -- Configuration specific key. The value should be modified if config structure was changed.
+#define CONFIG_VERSION "dem2"
+
+// -- When CONFIG_PIN is pulled to ground on startup, the Thing will use the initial
+//      password to buld an AP. (E.g. in case of lost password)
+#define CONFIG_PIN 17
+
+// -- Status indicator pin.
+//      First it will light up (kept LOW), on Wifi connection it will blink,
+//      when connected to the Wifi it will turn off (kept HIGH).
+#define STATUS_PIN LED_BUILTIN
+
+// -- Callback method declarations.
+void configSaved();
+boolean formValidator();
+
+DNSServer dnsServer;
+WebServer server(80);
+
+char icEndpointParamValue[STRING_LEN];
+char iCServerTokenParamValue[STRING_LEN];
+char pollingFrequencyParamValue[NUMBER_LEN];
+
+IotWebConf iotWebConf(thingName, &dnsServer, &server, wifiInitialApPassword, CONFIG_VERSION);
+IotWebConfSeparator separator1 = IotWebConfSeparator("Servidor de Integracion Continua");
+IotWebConfParameter icEndpointParam = IotWebConfParameter("Endpoint API REST Integracion Continua ", "icEndpointParam", icEndpointParamValue, STRING_LEN);
+// -- We can add a legend to the separator
+IotWebConfSeparator separator2 = IotWebConfSeparator("Autenticación del Repositorio de IC");
+IotWebConfParameter iCServerTokenParam = IotWebConfParameter("Token de Autenticación", "iCServerTokenParam", iCServerTokenParamValue, STRING_LEN);
+
+IotWebConfSeparator separator3 = IotWebConfSeparator("Configuración del Dispositivo");
+IotWebConfParameter pollingFrequencyParam = IotWebConfParameter("Periodo de Actualización (en segundos)", "pollingFrequencyParam", pollingFrequencyParamValue, NUMBER_LEN, "number", "1..3600", NULL, "min='1' max='3600' step='1'");
 
 
-//===============================================================
-// This routine is executed when you open its IP in browser
-//===============================================================
-void handleRoot() {
- String s = MAIN_page; //Read HTML contents
- server.send(200, "text/html", s); //Send web page
+/**
+ * Handle web requests to "/" path.
+ */
+void handleRoot()
+{
+  // -- Let IotWebConf test and handle captive portal requests.
+  if (iotWebConf.handleCaptivePortal())
+  {
+    // -- Captive portal request were already served.
+    return;
+  }
+
+  // -- HTML page fragments
+  String config_page  = "<!DOCTYPE html><html lang=\"en\"><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\"/><title>{v}</title>";
+  config_page += "<styles>.de{background-color:#ffaaaa;} .em{font-size:0.8em;color:#bb0000;padding-bottom:0px;} .c{text-align: center;} div,input{padding:5px;font-size:1em;} input{width:95%;} body{text-align: center;font-family:verdana;} button{border:0;border-radius:0.3rem;background-color:#16A1E7;color:#fff;line-height:2.4rem;font-size:1.2rem;width:100%;} fieldset{border-radius:0.3rem;margin: 0px;}</styles>";
+  config_page += "<script>function c(l){document.getElementById('s').value=l.innerText||l.textContent;document.getElementById('p').focus();}</script>";
+  config_page += "</head><body>";
+  config_page += "<div style='text-align:left;display:inline-block;min-width:260px;'>";
+  config_page += "<div>Haga click <a href='/config'>aqui</a> para configurar el dispositivo.</div>";
+  config_page+= "</div></body></html>";
+  
+  /*
+  String s = "<!DOCTYPE html><html lang=\"en\"><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\"/>";
+  s += "<title>IotWebConf 03 Custom Parameters</title></head><body>Hello world!";
+  s += "<ul>";
+  s += "<li>String param value: ";
+  s += stringParamValue;
+  s += "<li>Int param value: ";
+  s += atoi(intParamValue);
+  s += "<li>Float param value: ";
+  s += atof(floatParamValue);
+  s += "</ul>";
+  s += "Go to <a href='config'>configure page</a> to change values.";
+  s += "</body></html>\n";
+  */
+
+  server.send(200, "text/html", config_page);
 }
 
-//===============================================================
-// Setup
-//===============================================================
-
-void setup(void){
-  Serial.begin(115200);
-  Serial.println();
-  Serial.println("Booting Sketch...");
-  WifiManager* wifimanager = new WifiManager();
-  wifimanager.setup();
-  pinMode(14, OUTPUT);
-  pixels.clear(); 
-
-//ESP32 As access point IP: 192.168.4.1
-  WiFi.mode(WIFI_AP); //Access Point mode
-  WiFi.softAP("BalizaIC_A_B", "12345678");    //Password length minimum 8 char
-
-//Comment below code if you are using Access point only
-//ESP32 connects to your wifi -----------------------------------
-  WiFi.mode(WIFI_STA); //Connectto your wifi
-  WiFi.begin(ssid, password);
-
-  Serial.println("Connecting to ");
-  Serial.print(ssid);
-
-  //Wait for WiFi to connect
-  while(WiFi.waitForConnectResult() != WL_CONNECTED){      
-      Serial.print(".");
-      delay(1000);
-    }
-    
-  //If connection successful show IP address in serial monitor
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());  //IP address assigned to your ESP
-//----------------------------------------------------------------
- 
-  server.on("/", handleRoot);      //This is display page
- 
-  server.begin();                  //Start server
-  Serial.println("HTTP server started");
+void configSaved()
+{
+  Serial.println("Configuracion del dispositivo actualizada.");
 }
 
+void wifiConnected()
+{
+  Serial.println("Dispositivo conectado a wifi");
+  WIFI_SUCCESSFUL_CONNECTION = true;
+}
 
+boolean formValidator()
+{
+  Serial.println("Validating form.");
+  boolean valid = true;
+
+  int l = server.arg(icEndpointParam.getId()).length();
+  if (l < 3)
+  {
+    icEndpointParam.errorMessage = "Url invalida!";
+    valid = false;
+  }
+
+  return valid;
+}
 
 void changeLedLight(int r , int g, int b){
 
@@ -120,86 +158,123 @@ void changeLedLight(int r , int g, int b){
 void getTravisInfo(){
 
   String payload;
+  HTTPClient http;
+  http.begin(icEndpointParamValue); //Specify the URL
+  http.addHeader("Travis-API-Version", "3");
+  String token_str = "token ";
+  token_str += String(iCServerTokenParamValue);
+  Serial.println(token_str);
+  http.setAuthorization(token_str.c_str());
+  
+  int httpCode = http.GET();  //Make the request
 
-  if ((WiFi.status() == WL_CONNECTED)) { //Check the current connection status
- 
-    HTTPClient http;
- 
-    http.begin("https://api.travis-ci.org/repo/25849455/builds?limit=1"); //Specify the URL
-    http.addHeader("Travis-API-Version", "3");
-    http.setAuthorization("token sN-LuhyiLh61-Ya2zK-2Xw");
+  if (httpCode == 200) { //Check for the returning code
+
+    payload = http.getString();
+    Serial.print("Status Code: ");
+    Serial.println(httpCode);
+    //Serial.println(payload);
+    // Allocate the JSON document
+    // Use arduinojson.org/v6/assistant to compute the capacity.
+    const size_t capacity = JSON_ARRAY_SIZE(0) + 2*JSON_ARRAY_SIZE(1) + 4*JSON_OBJECT_SIZE(3) + 2*JSON_OBJECT_SIZE(4) + 3*JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(6) + JSON_OBJECT_SIZE(8) + JSON_OBJECT_SIZE(9) + JSON_OBJECT_SIZE(23) + 1540;
     
-    int httpCode = http.GET();  //Make the request
- 
-    if (httpCode > 0) { //Check for the returning code
+    DynamicJsonDocument doc(capacity);
 
-      payload = http.getString();
-      Serial.print("Status Code: ");
-      Serial.println(httpCode);
-      //Serial.println(payload);
-    }
-    else {
-      Serial.println("Error on HTTP request");
+    // Parse JSON object
+    DeserializationError error = deserializeJson(doc, payload);
+    if (error) {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.c_str());
+      return;
     }
 
-    http.end(); //Free the resources
-  }
-  
-  // Allocate the JSON document
-  // Use arduinojson.org/v6/assistant to compute the capacity.
-  const size_t capacity = JSON_ARRAY_SIZE(0) + 2*JSON_ARRAY_SIZE(1) + 4*JSON_OBJECT_SIZE(3) + 2*JSON_OBJECT_SIZE(4) + 3*JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(6) + JSON_OBJECT_SIZE(8) + JSON_OBJECT_SIZE(9) + JSON_OBJECT_SIZE(23) + 1540;
-  
-  DynamicJsonDocument doc(capacity);
+    JsonObject builds_0 = doc["builds"][0];
+    const char* builds_0_state = builds_0["state"]; // "passed"
+    long builds_0_id = builds_0["id"]; // 595914427 
+    // Extract values
 
-  // Parse JSON object
-  DeserializationError error = deserializeJson(doc, payload);
-  if (error) {
-    Serial.print(F("deserializeJson() failed: "));
-    Serial.println(error.c_str());
-    return;
-  }
-  JsonObject builds_0 = doc["builds"][0];
-  const char* builds_0_state = builds_0["state"]; // "passed"
-  long builds_0_id = builds_0["id"]; // 595914427 
-  // Extract values
+    Serial.println(F("Estado del ultimo build:"));
+    Serial.print(builds_0_id);
+    Serial.print(":");
+    Serial.println(builds_0_state);
+    String status_passed = "passed";
+    String status_started = "started";
+    String status_failed = "failed";
+    String status_canceled = "canceled";
+    
+    if(status_passed.equals(builds_0_state)){
+      changeLedLight(0, 255, 0);
+      Serial.println("Esta en passed!");
+    }
 
-  Serial.println(F("Estado del ultimo build:"));
-  Serial.print(builds_0_id);
-  Serial.print(":");
-  Serial.println(builds_0_state);
-  String status_passed = "passed";
-  String status_started = "started";
-  String status_failed = "failed";
-  String status_canceled = "canceled";
-  if(status_passed.equals(builds_0_state)){
-    changeLedLight(0, 255, 0);
-    Serial.println("Esta en passed!");
-  }
+    if(status_started.equals(builds_0_state)){
+      changeLedLight(255,255,0);
+      Serial.println("Esta en started!");
+    }
 
-  if(status_started.equals(builds_0_state)){
-    changeLedLight(255,255,0);
-    Serial.println("Esta en started!");
+    if(status_canceled.equals(builds_0_state)){
+      changeLedLight(192,192,192);
+      Serial.println("Esta en canceled!");
+    }
+    
+    if(status_failed.equals(builds_0_state)){
+      changeLedLight(255,0,0);
+      Serial.println("Esta en failed!");
+    }
   }
-
-  if(status_canceled.equals(builds_0_state)){
-    changeLedLight(192,192,192);
-    Serial.println("Esta en canceled!");
-  }
-  
-  if(status_failed.equals(builds_0_state)){
-    changeLedLight(255,0,0);
-    Serial.println("Esta en failed!");
+  else {
+    Serial.println("Error on HTTP request");
   }
 
+  http.end(); //Free the resources
 }
 
 
-//===============================================================
-// This routine is executed when you open its IP in browser
-//===============================================================
-void loop(void){
-  server.handleClient();
+
+void setup() 
+{
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("Starting up...");
+
+  iotWebConf.setStatusPin(STATUS_PIN);
+  iotWebConf.setConfigPin(CONFIG_PIN);
+  iotWebConf.addParameter(&icEndpointParam);
+  iotWebConf.addParameter(&separator1);
+  iotWebConf.addParameter(&iCServerTokenParam);
+  iotWebConf.addParameter(&separator3);
+  iotWebConf.addParameter(&pollingFrequencyParam);
+  iotWebConf.setConfigSavedCallback(&configSaved);
+  iotWebConf.setFormValidator(&formValidator);
+  iotWebConf.getApTimeoutParameter()->visible = true;
+
+  // -- Initializing the configuration.
+  boolean validConfig = iotWebConf.init();
+  if (!validConfig)
+  {
+    icEndpointParamValue[0] = '\0';
+  }
+
+  // -- Set up required URL handlers on the web server.
+  server.on("/", handleRoot);
+  server.on("/config", []{ iotWebConf.handleConfig(); });
+  server.onNotFound([](){ iotWebConf.handleNotFound(); });
+
+  Serial.println("Lista la baliza.");
+
+  pinMode(PIN, OUTPUT);
+  pixels.clear(); 
+}
+
+void loop() 
+{
+  // -- doLoop should be called as frequently as possible.
+
+  iotWebConf.doLoop();
+
   getTravisInfo();
   //changeLedLight();
-  delay(300);
+  String delay_str = String(pollingFrequencyParamValue);
+  //iotWebConf.delay(delay_str.toInt()*1000);
+  delay(1000);
 }
